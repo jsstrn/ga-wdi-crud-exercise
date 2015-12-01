81ff66dbc9738e0bb6ea515cd8c1818b2282ec5e
jsstrn <jessternrays@gmail.com>
2015-12-01 15:31:41 +0800

    Update files

--- app/app.js
+++ app/app.js
@@ -1,6 +1,24 @@
 const express = require('express')
-const scoreboard = require('../data/scoreboard.json')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+
+// connect to database
+const dbuser = process.env.SCOREBOARD_DB_USER
+const dbpassword = process.env.SCOREBOARD_DB_PASSWORD
+const dburl = 'mongodb://' + dbuser + ':' + dbpassword + '@ds061954.mongolab.com:61954/scoreboard'
+mongoose.connect(dburl)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error: '))
+
+// define mongoose model (schema)
+const Score = mongoose.model('Score', {
+  "player": String,
+  "score": Number
+})
+
+// read local data
+const scoreboard = require('../data/scoreboard.json')
+
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -17,9 +35,13 @@ app.get('/scores/:id', (req, res) => {
 
 // create
 app.post('/scores', (req, res) => {
-  const score = req.body
-  scoreboard.push(score)
-  res.json(score)
+  const score = new Score(req.body)
+  score.save(err => {
+    if (err) return console.error(err)
+    console.log('New score added.')
+    res.json(req.body)
+  })
+  // scoreboard.push(req.body)
 })
 
 // update
@@ -30,7 +52,7 @@ app.put('/scores/:id', (req, res) => {
 
 // delete
 app.delete('/scores/:id', (req, res) => {
-  delete scoreboard[req.params.id]
+  delete scoreboard[Number(req.params.id)]
   res.send(scoreboard)
 })
 
